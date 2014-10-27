8997a213d7466b35c075dbd9fcd8ac55fa0f3f9a
unaveed <unaveed@gmail.com>
2014-10-27 14:59:43 -0600

    Add all files

---
+++ app/src/main/java/edu/utah/cs4962/battleship/BattleshipActivity.java
@@ -0,0 +1,14 @@
+package edu.utah.cs4962.battleship;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class BattleshipActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_battleship);
+    }
+}
